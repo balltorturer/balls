091561ab5d4c14433e9e6f9fa31efa8fbe7cd8b2
balltorturer <kq4nje@yandex.ru>
2023-10-22 02:41:12 +0300

    homewowk

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника", smax)
+#Задача: На вход программе подаются 3 коэффициента квадратного
+#уравнения. Программа должна находить корни квадратного уравнения.
+#Залить данную задачу на локальный Git и GitHub.
+import math
+print("Введите коэффициенты уравнения")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+if b ** 2 - 4 * a * c > 0:
+    x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    print("x1 =",x1,"x2 = ",x2)
+elif b ** 2 - 4 * a * c == 0:
+    x = -b / (2 * a)
+    print("x =",x)
+else:
+    print("Корней нет")
