67dc3298e72845b03992304ed5d230b2fa0ef98c
balltorturer <kq4nje@yandex.ru>
2023-10-22 04:50:28 +0300

    wetha alpha

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+appid="fea9623caa0d853373e467a102dfe523"
+s_city="Moscow,RU"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+#print("Скорость ветра",data[''][''])
+#print("Видимость",[''][''])
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+'{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+i['weather'][0]['description'], ">")
+print("____________________________")
